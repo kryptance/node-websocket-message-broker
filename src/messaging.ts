5cbaeb9f9fe26db7d17c7be6b78d8331e3aeb098
Rising Star <star-atnight@outlook.com>
2022-01-17 17:31:57 +0800

    Add the error handler for failed push subscriptions

--- src/messaging.ts
+++ src/messaging.ts
@@ -25,9 +25,19 @@ export function deliverMessage(subscription: Subscriber, sockets: Map<string, So
         });
 
         webpush.sendNotification(subscription.webPush, payload)
-            .then(result => console.log(''))
+            .then(() => console.log('Successfully sent.'))
             .catch(e => {
                 console.log(e.stack)
+
+                // Remove the failed subscriptions
+                const auth = subscription.webPush.keys.auth
+                const subsForDestination = subscriptions.getValue(message.destination as string)
+                subscriptions.remove(message.destination as string)
+                
+                subsForDestination
+                    .filter(sub => (sub.webPush.keys.auth !== auth))
+                    .map(sub => subscriptions.setValue(message.destination as string, sub))
+                console.log(subscriptions.getValue(message.destination as string))
             });
     }
 }
